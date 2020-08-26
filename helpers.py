e1bc5ca1d4fda92cebdbcb14e3248d16dd00ef3b
utytlanyjoe <daiusz.lewandowski@ivytech.com>
2020-08-26 07:43:27 +0200

    Rounding values in convert_to_human_readable funciton

--- helpers.py
+++ helpers.py
@@ -4,25 +4,25 @@ def convert_to_human_readable(size, use_exact_size=False):
         size=int(size)
         if use_exact_size:
             if size > 1099511627776:
-                return "{} TB".format(int(size / 1099511627776))
+                return "{} TB".format(int(round(size / 1099511627776)))
             elif size > 1073741824:
-                return "{} GB".format(int(size / 1073741824))
+                return "{} GB".format(int(round(size / 1073741824)))
             elif size > 1048576:
-                return "{} MB".format(int(size / 1048576))
+                return "{} MB".format(int(round(size / 1048576)))
             elif size > 1024:
-                return "{} KB".format(int(size / 1024))
+                return "{} KB".format(int(round(size / 1024)))
             else:
                 return "{} B".format(int(size))
         else:
             if size >  1000000000000:
-                return "{} TB".format(int(size/1000000000000))
+                return "{} TB".format(int(round(size/1000000000000)))
             elif size > 1000000000:
-                return "{} GB".format(int(size / 1000000000))
+                return "{} GB".format(int(round(size / 1000000000)))
             elif size > 1000000:
-                return "{} MB".format(int(size / 1000000))
+                return "{} MB".format(round(int(size / 1000000)))
             elif size > 1000:
-                return "{} KB".format(int(size / 1000))
+                return "{} KB".format(round(int(size / 1000)))
             else:
-                return "{} B".format(int(size))
+                return "{} B".format(round(int(size)))
     except Exception as e:
         return None
